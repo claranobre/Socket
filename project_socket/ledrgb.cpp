d5330110c9bc105364654d728224a38abb96518a
claranobre <aclaranobre@gmail.com>
2016-04-07 17:49:20 -0300

    alterando algoritmo da led

--- project_socket/ledrgb.cpp
+++ project_socket/ledrgb.cpp
@@ -1,3 +1,46 @@
+/*
+gpio_initialized = 0;
+
+initialized_gpio_leds();
+
+GPIO.setup("P8_8", GPIO.OUT);
+GPIO.setup("P8_1O", GPIO.OUT);
+
+gpio_initialized = 1;
+*/
+
+
+
+/*
+ #include <iostream>
+ #include <stdio.h>
+ #include <unistd.h>
+ using namespace std;
+
+ int main(){
+ cout << "LED Flash Start" << endl;
+ FILE *LEDHandle = NULL;
+ const char *LEDBrightness="/sys/class/leds/beaglebone:green:usr0/brightness";
+
+ for(int i=0; i<10; i++){
+    if((LEDHandle = fopen(LEDBrightness, "r+")) != NULL){
+        fwrite("1", sizeof(char), 1, LEDHandle);
+        fclose(LEDHandle);
+    }
+    usleep(1000000);
+
+    if((LEDHandle = fopen(LEDBrightness, "r+")) != NULL){
+        fwrite("0", sizeof(char), 1, LEDHandle);
+        fclose(LEDHandle);
+    }
+    usleep(1000000);
+  }
+  cout << "LED Flash End" << endl;
+ }
+*/
+
+
+
 //Carrega a biblioteca bonescript
 var b = require('bonescript');
 var contador = 0;
